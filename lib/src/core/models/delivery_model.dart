babb02a985e20766d44c75b85c2171e196a18859
Okai Yeboah <kingkloy16@gmail.com>
2020-12-26 05:09:14 +0000

    add delivery data model

---
+++ lib/src/core/models/delivery_model.dart
@@ -0,0 +1,68 @@
+import 'package:equatable/equatable.dart';
+
+// ignore: must_be_immutable
+class DeliveryModel extends Equatable {
+  int id;
+  int customerId;
+  String totalPrice;
+  String smallBreadQty;
+  String bigBreadQty;
+  String biggerBreadQty;
+  String biggestBreadQty;
+  String roundBreadQty;
+  String deliveryDate;
+
+  DeliveryModel(
+      {this.id,
+      this.customerId,
+      this.totalPrice,
+      this.smallBreadQty,
+      this.bigBreadQty,
+      this.biggerBreadQty,
+      this.biggestBreadQty,
+      this.roundBreadQty,
+      this.deliveryDate});
+
+  // converts delivery details to a Map
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      'id': id,
+      'customerId': customerId,
+      'totalPrice': totalPrice,
+      'smallBreadQty': smallBreadQty,
+      'bigBreadQty': bigBreadQty,
+      'biggerBreadQty': biggerBreadQty,
+      'biggestBreadQty': biggestBreadQty,
+      'roundBreadQty': roundBreadQty,
+      'deliveryDate': deliveryDate,
+    };
+
+    return map;
+  }
+
+  // destruct map of delivery model
+  DeliveryModel.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    customerId = map['customerId'];
+    totalPrice = map['totalPrice'];
+    smallBreadQty = map['smallBreadQty'];
+    bigBreadQty = map['bigBreadQty'];
+    biggerBreadQty = map['biggerBreadQty'];
+    biggestBreadQty = map['biggestBreadQty'];
+    roundBreadQty = map['roundBreadQty'];
+    deliveryDate = map['deliveryDate'];
+  }
+
+  @override
+  List<Object> get props => [
+        id,
+        customerId,
+        totalPrice,
+        smallBreadQty,
+        bigBreadQty,
+        biggerBreadQty,
+        biggestBreadQty,
+        roundBreadQty,
+        deliveryDate
+      ];
+}
