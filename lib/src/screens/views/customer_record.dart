4882c4ce3f8a9be23cd13f90b9936c991be8ed64
Okai Yeboah <kingkloy16@gmail.com>
2021-01-11 01:45:59 +0000

    add customer record screen

---
+++ lib/src/screens/views/customer_record.dart
@@ -0,0 +1,238 @@
+import 'dart:io';
+
+import 'package:benevolence_calculator/src/core/bloc/customer_record_bloc.dart';
+import 'package:benevolence_calculator/src/core/models/delivery_model.dart';
+import 'package:benevolence_calculator/src/core/models/payment_model.dart';
+import 'package:benevolence_calculator/src/screens/components/loading.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CustomerRecord extends StatefulWidget {
+  final int customerId;
+
+  const CustomerRecord({Key key, this.customerId}) : super(key: key);
+
+  @override
+  _CustomersState createState() => _CustomersState();
+}
+
+class _CustomersState extends State<CustomerRecord> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CustomerRecordBloc(),
+      child: DefaultTabController(
+        length: 2,
+        child: BlocBuilder<CustomerRecordBloc, CustomerRecordState>(
+            builder: (context, state) {
+          if (state is CustomerRecordInitial) {
+            BlocProvider.of<CustomerRecordBloc>(context)
+                .add(GetCustomerRecord(customerId: widget.customerId));
+            return Scaffold(body: Loading());
+          } else if (state is CustomerRecordLoading) {
+            return Scaffold(body: Loading());
+          } else if (state is CustomerRecordLoaded) {
+            debugPrint("${state.customerRecord["deliveries"]}");
+            return Scaffold(
+              appBar: AppBar(
+                title: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    state.customerRecord["customer"].imagePath.isEmpty
+                        ? Container(
+                            margin: EdgeInsets.only(right: 10.0),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: AssetImage("assets/user-profile.png")),
+                            ),
+                            height: 30,
+                            width: 30,
+                          )
+                        : ClipOval(
+                            child: Image.file(
+                              File(state.customerRecord["customer"].imagePath),
+                              height: 30,
+                              width: 30,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                    Text("${state.customerRecord["customer"].name}"),
+                  ],
+                ),
+                bottom: TabBar(
+                  tabs: [
+                    Tab(
+                        child: Text('Deliveries',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6
+                                .merge(TextStyle(color: Colors.white)))),
+                    Tab(
+                        child: Text('Payments',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6
+                                .merge(TextStyle(color: Colors.white))))
+                  ],
+                ),
+              ),
+              body: TabBarView(children: [
+                SingleChildScrollView(
+                  child:
+                      _renderDeliveriesView(state.customerRecord["deliveries"]),
+                ),
+                SingleChildScrollView(
+                  child: _renderPaymentsView(state.customerRecord["payments"]),
+                ),
+              ]),
+            );
+          } else
+            return Scaffold(body: Loading());
+        }),
+      ),
+    );
+  }
+
+  _renderPaymentsView(List<PaymentModel> payments) {
+    if (payments.isEmpty) return Center(child: Text("No payments yet"));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+          children:
+              payments.map((payment) => _buildPaymentCard(payment)).toList()),
+    );
+  }
+
+  _renderDeliveriesView(List<DeliveryModel> deliveries) {
+    if (deliveries.isEmpty) return Center(child: Text("No deliveries yet"));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+          children: deliveries
+              .map((delivery) => _buildDeliveryCard(delivery))
+              .toList()),
+    );
+  }
+
+  Card _buildPaymentCard(PaymentModel paymentModel) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 20.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
+        ),
+      ),
+      elevation: 2.0,
+      child: ExpansionTile(
+        backgroundColor: Colors.white,
+        childrenPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25),
+        title: Text(
+          '${paymentModel.paymentDate}',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Amount'),
+              Text(
+                '${paymentModel.amount}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Card _buildDeliveryCard(DeliveryModel deliveryModel) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 20.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
+        ),
+      ),
+      elevation: 2.0,
+      child: ExpansionTile(
+        backgroundColor: Colors.white,
+        childrenPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25),
+        title: Text(
+          '${deliveryModel.deliveryDate}',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Total price'),
+              Text(
+                '${deliveryModel.totalPrice}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              'Bread Quantities',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Small (GH¢2.00 each)'),
+              Text(
+                '${deliveryModel.smallBreadQty}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Big (GH¢2.50 each)'),
+              Text(
+                '${deliveryModel.bigBreadQty}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Bigger (GH¢3.00 each)'),
+              Text(
+                '${deliveryModel.biggerBreadQty}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Biggest (GH¢4.00 each)'),
+              Text(
+                '${deliveryModel.biggestBreadQty}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Round (GH¢5.00 per set)'),
+              Text(
+                '${deliveryModel.roundBreadQty}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
