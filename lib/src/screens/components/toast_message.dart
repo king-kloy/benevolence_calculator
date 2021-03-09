7984834bb2f7aaab5cab353f4c96d9f0f2c815d5
Okai Yeboah <kingkloy16@gmail.com>
2021-03-09 00:19:35 +0000

    add toast message helper function

---
+++ lib/src/screens/components/toast_message.dart
@@ -0,0 +1,27 @@
+import 'package:flash/flash.dart';
+import 'package:flutter/material.dart';
+
+toastMessage(BuildContext context, String message, [Color color]) {
+  showFlash(
+    context: context,
+    duration: Duration(seconds: 2),
+    builder: (context, controller) {
+      return Flash(
+        backgroundColor: color ?? Colors.green,
+        controller: controller,
+        style: FlashStyle.floating,
+        boxShadows: kElevationToShadow[4],
+        horizontalDismissDirection: HorizontalDismissDirection.horizontal,
+        child: FlashBar(
+          message: Text(
+            message,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+    },
+  );
+}
+
+
+// how do you know you're still here
